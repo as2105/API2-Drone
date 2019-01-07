72d74d1176379e9a5dddff406969cd2ecef25c00
Steve Ruckdashel <steven.ruckdashel@optum.com>
2019-01-07 13:58:00 -0600

    cleanup

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -45,17 +45,6 @@ func RegisterHealthCheckRoutes(r *mux.Router, log *logging.Logger) {
 func RegisterPProfRoutes(r *mux.Router, log *logging.Logger) {
 	log.Debug("executing RegisterPProfRoutes")
 	r.PathPrefix("/debug").Handler(http.DefaultServeMux)
-
-	// r.HandleFunc("/debug/pprof/", pprof.Index)
-	// r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	// r.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	// r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	// r.HandleFunc("/debug/pprof/trace", pprof.Trace)
-
-	// r.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
-	// r.Handle("/debug/pprof/heap", pprof.Handler("heap"))
-	// r.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
-	// r.Handle("/debug/pprof/block", pprof.Handler("block"))
 }
 
 func registerFHIRResourceRoutes(r *mux.Router, log *logging.Logger, rndr *render.Render, i interface{}) {
